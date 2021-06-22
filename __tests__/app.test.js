9c46e35031e38ea775d9546c929cc29824ad2ee6
austinxduong <axduong2@gmail.com>
2021-06-22 02:39:51 -0700

    setup failed test for .post, work vertically next

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,9 +3,15 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('Amazon S3 storage buckets routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 });
 
+it('it creates and/or configures a bucket, via communicating to .PUT route in controller', async () => {
+  const res = await request(app)
+    .post('/api/v1/clouds')
+    // .send({ Key: value });
+  expect(res.body).toEqual({ key: 'value', key: value });
+});
