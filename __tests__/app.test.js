994ea88416c4695b309aa4a96cb72261e4deea91
austinxduong <axduong2@gmail.com>
2021-06-25 01:25:16 -0700

    created .GET(findbyAll) route, expected 404 error

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Cloud from '../lib/models/cloud.js';
 // import Cloud from '../lib/models/Cloud.js';
 
 describe('Amazon S3 storage buckets routes', () => {
@@ -30,4 +31,25 @@ describe('Amazon S3 storage buckets routes', () => {
     });
   });
 
+  test('gets a bucket via .GET', async () => {
+    const bucket1 = await Cloud.insert({
+      bucket: 'this bucket holds files',
+      object: 'this is a .JPG file, living inside the bucket'
+    });
+
+    const bucket2 = await Cloud.insert({
+      bucket: 'this is my second bucket, that holds files',
+      object: 'this will store my notes, all are .pdf files'
+    });
+
+    const bucket3 = await Cloud.insert({
+      bucket: 'this is my third bucket, that holds files',
+      object: 'i will store all my memories, all are .mov files'
+    });
+
+    const res = await request(app)
+      .get('/api/v1/clouds/');
+  
+    expect(res.body).toEqual([bucket1, bucket2, bucket3]);
+  });
 });
